f3054ee5a2b9916d85f56676ff4ed188ee27891c
Epistaxiophobic <xerotheprogrammer@gmail.com>
2024-11-21 12:04:32 +0330

    add file reading and stdin support

--- huffer/__main__.py
+++ huffer/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+from os.path import exists, isfile
+from sys import stdin
 
 def main():
     # Add command line arguments
@@ -6,6 +8,21 @@ def main():
     parser.add_argument("MDOE", type=str, help="the operation mode of the program, e for encode / d for decode")
     parser.add_argument("FILE", type=str, nargs="?", help="if FILE is - or no file is specified read standard input")
     args = parser.parse_args()
+
+    data = ""
+
+    if args.FILE is None or args.FILE == "-":
+        data = stdin.read()
+    else:
+        if not exists(args.FILE):
+            print(f"No such file or directory: {args.FILE}")
+            quit()
+        if not isfile(args.FILE):
+            print(f"{args.FILE}: is not a file")
+            quit()
+        with open(args.FILE, "r") as fh:
+            for line in fh:
+                data += line
 
 if __name__ == "__main__":
     main()
